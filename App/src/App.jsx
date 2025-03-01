195fad48137db1e312ad184088a448fd3c0f991b
mooojn <moojntariq@gmail.com>
2025-03-01 16:37:44 +0500

    fix app ref

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,46 +1,17 @@
-import { useState, useEffect } from "react";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import the component
+
+import Home from './Home.jsx';
+
+library.add(fas, faTwitter, faFontAwesome);
 
 function App() {
-  const [message, setMessage] = useState("");
-  const [recMessage, setRecMessage] = useState("Loading...");
-
-  useEffect(() => {
-    fetch("http://localhost:8080")
-      .then((response) => response.json()) 
-      .then((data) => setRecMessage(data.message)) 
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
-  const sendDataToServer = async () => {
-    const data = { message }; 
-
-    const response = await fetch("http://localhost:8080/save", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    const result = await response.json();
-    alert(result.message); 
-  };
-
   return (
-    <div>
-     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Message</h1>
-      <p>{recMessage}</p>
-    </div>
-      <h1>Send Data to C Backend</h1>
-      <input
-        type="text"
-        placeholder="Enter message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button onClick={sendDataToServer}>Send</button>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
